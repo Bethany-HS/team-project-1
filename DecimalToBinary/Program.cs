781a37ecb0cdaa3b9747e13c5e8f12239ca4511b
Bethany Huhtala-Schoonover <betheth614@gmail.com>
2021-01-15 22:14:27 -0500

    binary complete

--- DecimalToBinary/Program.cs
+++ DecimalToBinary/Program.cs
@@ -6,7 +6,24 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.Write("Please enter in a series of decimal values (separated by spaces): ");
+            string[] decimalValues = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i = 0; i < decimalValues.Length; i++)
+            {
+                string reversePolishNotation = "";
+                int baseTen = int.Parse(decimalValues[i]);
+                while (baseTen > 0)
+                {
+                    int remainder = baseTen % 2;
+                    baseTen = baseTen / 2;
+                    reversePolishNotation = remainder + reversePolishNotation;
+                }
+                Console.WriteLine($"{decimalValues[i]} in binary is {reversePolishNotation}");
+            } 
+        } 
+
+      
+
     }
 }
